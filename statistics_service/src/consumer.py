b54f159a0b9b10539293fa79ea63f99d1f346c41
Kirill-Stepankov <kirillstepankov18@gmail.com>
2023-11-14 13:08:34 +0300

    kafka consumer

--- statistics_service/src/consumer.py
+++ statistics_service/src/consumer.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 from contextlib import asynccontextmanager
 
 from aiokafka import AIOKafkaConsumer
@@ -11,30 +12,23 @@ from .config import get_settings
 settings = get_settings()
 logger = get_logger()
 
-loop = asyncio.get_event_loop()
-consumer = AIOKafkaConsumer(
-    settings.kafka_topic, bootstrap_servers=settings.bootstrap_servers, loop=loop
-)
 
+class Consumer:
+    def __init__(self, loop, kafka_consumer) -> None:
+        self.loop = loop
+        self.consumer = kafka_consumer
 
-async def consume():
-    logger.info("Starting consuming..")
-    await consumer.start()
-    async for msg in consumer:
-        print(
-            "consumed: ",
-            msg.topic,
-            msg.partition,
-            msg.offset,
-            msg.key,
-            msg.value,
-            msg.timestamp,
-        )
+    async def consume(self):
+        logger.info("Starting consuming..")
+        await self.consumer.start()
+        async for msg in self.consumer:
+            json_string = msg.value.decode("utf-8").replace("'", '"')
+            payload = json.loads(json_string)
+            print(payload)
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    loop.create_task(consume())
-    yield
-    logger.info("Stopping consuming")
-    await consumer.stop()
+    @asynccontextmanager
+    async def lifespan(self, app: FastAPI):
+        self.loop.create_task(self.consume())
+        yield
+        logger.info("Stopping consuming")
+        await self.consumer.stop()
--- statistics_service/src/main.py
+++ statistics_service/src/main.py
@@ -1,18 +1,29 @@
+import asyncio
 from functools import partial
 from typing import Annotated
 
+from aiokafka import AIOKafkaConsumer
 from fastapi import Depends, FastAPI
 from src.pages.dependencies import pages_stats_service
 from src.pages.router import router
 from src.pages.service import AbstractPagesStatisticsService
 
 from . import config
-from .consumer import lifespan
+from .consumer import Consumer
 from .database import get_db
+
+settings = config.get_settings()
 
 
 def create_app():
-    app = FastAPI(lifespan=lifespan)
+    loop = asyncio.get_event_loop()
+    consumer = AIOKafkaConsumer(
+        settings.kafka_topic, bootstrap_servers=settings.bootstrap_servers, loop=loop
+    )
+
+    consumer = Consumer(loop, consumer)
+
+    app = FastAPI(lifespan=consumer.lifespan)
 
     app.include_router(router)
 
