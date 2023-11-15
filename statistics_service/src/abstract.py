cfae9708047d4cecbc95ddef82c7007985fa5db5
Kirill-Stepankov <kirillstepankov18@gmail.com>
2023-11-15 17:04:07 +0300

    delete useless repository methods

--- statistics_service/src/abstract.py
+++ statistics_service/src/abstract.py
@@ -5,15 +5,3 @@ class AbstractRepository(ABC):
     @abstractmethod
     async def add_one():
         pass
-
-    @abstractmethod
-    async def find():
-        pass
-
-    @abstractmethod
-    async def get():
-        pass
-
-    @abstractmethod
-    async def delete():
-        pass
